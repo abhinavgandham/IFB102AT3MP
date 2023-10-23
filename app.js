7efb7c9bfc7194a09e32bac9d3989b1b8ebc9b41
Abhinav Gandham <abhinavgandham@gmail.com>
2023-10-23 10:42:44 +1000

    just a commit

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require("path");
 require("dotenv").config();
 const port = process.env.PORT;
+const fs = require("fs");
+const https = require("https");
 
 // -------Defining the route - req = Request Object, res = Response Object-------
 //  Serving the HTML file
@@ -18,3 +20,59 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+// In your server-side app.js
+app.get("/api/access-token", (req, res) => {
+  res.json({ accessToken: process.env.accessToken });
+});
+
+const postData = `refresh_token=8MmYJjmPPFMAAAAAAAAAAYsDGnf7fC655d9G1VhrSCgv5lI9gCEeVr82ZGTujgES&grant_type=refresh_token&client_id=0dc8hbklx489vn5&client_secret=h870pkmfatftvzg`;
+
+const options = {
+  hostname: "api.dropbox.com",
+  path: "/oauth2/token",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Content-Length": postData.length,
+  },
+};
+
+const req = https.request(options, (res) => {
+  let data = "";
+
+  res.on("data", (chunk) => {
+    data += chunk;
+  });
+
+  res.on("end", () => {
+    // Parse the response JSON to get the access_token
+    const response = JSON.parse(data);
+    const accessToken = response.access_token;
+
+    // Update the .env file
+    fs.readFile(".env", "utf8", (err, envContents) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const updatedEnv = envContents.replace(
+          /(accessToken=)(.*)/,
+          `accessToken=${accessToken}`
+        );
+        fs.writeFile(".env", updatedEnv, (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log("Access token updated successfully.");
+          }
+        });
+      }
+    });
+  });
+});
+
+req.on("error", (error) => {
+  console.error(error);
+});
+
+req.write(postData);
+req.end();
--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-// Boiler plate code to display editor and for save/load functionality
 document.addEventListener("DOMContentLoaded", function () {
   // Initialize TinyMCE
   tinymce.init({
     selector: "textarea#editor",
-    height: 500,
-    plugins: "save",
-    toolbar: "save",
-    save_enablewhendirty: true,
-    save_onsavecallback: saveContent,
+    height: 400,
+    plugins:
+      "preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons accordion",
+    menubar: "file edit view insert format tools table help",
+    toolbar:
+      "undo redo | accordion accordionremove | blocks fontfamily fontsize | bold italic underline strikethrough | align numlist bullist | link image | table media | lineheight outdent indent| forecolor backcolor removeformat | charmap emoticons | code fullscreen preview | save print | pagebreak anchor codesample | ltr rtl",
+    autosave_ask_before_unload: true,
+    autosave_interval: "30s",
+    autosave_restore_when_empty: false,
+    autosave_retention: "2m",
+    image_advtab: true,
+    image_caption: true,
+    quickbars_selection_toolbar:
+      "bold italic | quicklink h2 h3 blockquote quickimage quicktable",
+    toolbar_mode: "sliding",
+    contextmenu: "link image table",
   });
-
-  // Dropbox access token
-  const accessToken =
-    "sl.BngIi5CzWAUQ8m041hSseJc2W8GzNjn7V9ZtZlwrjhyzX8h-mT5-p95GhTUnN2V6sjEV7GW7_z9yT1OjnZAIPSuLLnZhGBsHPdb7ECf82X-k7A9to5D-6J5otsbKNfy7EjhF-XRSRqZ5";
 
   // Save content to Dropbox
   document.getElementById("save-button").addEventListener("click", function () {
@@ -23,73 +29,165 @@ document.addEventListener("DOMContentLoaded", function () {
     const content = tinymce.activeEditor.getContent();
     const blob = new Blob([content], { type: "text/html" });
     const file = new File([blob], "document.html");
+    const refreshButton = document.getElementById("refreshButton");
+    const filenameInput = document.getElementById("filenameInput");
+    const inputValue = filenameInput.value.trim();
+    const timestamp = new Date().toLocaleString().replace(/[^0-9]/g, "");
+
+    const filename = inputValue ? inputValue : `document${timestamp}`;
 
     const formData = new FormData();
     formData.append("file", file);
+    fetch("/api/access-token")
+      .then((response) => response.json())
+      .then((data) => {
+        let accessToken = data.accessToken;
 
-    fetch("https://content.dropboxapi.com/2/files/upload", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Dropbox-API-Arg": JSON.stringify({
-          path: "/document.html",
-          mode: "overwrite",
-        }),
-        "Content-Type": "application/octet-stream",
-      },
-      body: file,
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log("File saved to Dropbox successfully.");
-          // --------My added on feature - Displaying a message when file is saved to dropbox------
-          message.textContent = "File saved successfully!";
-          message.style.textAlign = "center";
-          message.style.color = "#fff";
-          bodySection.append(message);
-        } else {
-          console.error("Error saving file to Dropbox:", response.statusText);
-          // -------My added on feature - Displaying an error message if there is an error--------
-          message.textContent = "Something went wrong ☹️";
-          message.style.textAlign = "center";
-          message.style.color = "#fff";
-          bodySection.append(message);
-        }
+        // Now you can use the accessToken in your client-side code
+        fetch("https://content.dropboxapi.com/2/files/upload", {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Dropbox-API-Arg": JSON.stringify({
+              // path: "/htmls/${filename}.html",
+              path: `/htmls/${filename}.html`, // Use backticks to interpolate filename
+              mode: "add",
+            }),
+            "Content-Type": "application/octet-stream",
+          },
+          body: file,
+        })
+          .then((response) => {
+            if (response.ok) {
+              console.log("File saved to Dropbox successfully.");
+              alert("File saved to Dropbox successfully.");
+              filenameInput.value = "";
+            } else {
+              console.error(
+                "Error saving file to Dropbox:",
+                response.statusText
+              );
+              alert(
+                "Error saving file to Dropbox - Check Console For Error Status"
+              );
+            }
+          })
+          .catch((error) => {
+            console.error("Error saving file to Dropbox:", error);
+            alert(
+              "Error saving file to Dropbox - Check Console For Error Status"
+            );
+          });
       })
       .catch((error) => {
-        console.error("Error saving file to Dropbox:", error);
+        console.error("Error:", error);
       });
   });
 
   // Load content from Dropbox
   document.getElementById("load-button").addEventListener("click", function () {
-    fetch("https://content.dropboxapi.com/2/files/download", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Dropbox-API-Arg": JSON.stringify({
-          path: "/document.html",
-        }),
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.text();
-        } else {
-          console.error(
-            "Error loading file from Dropbox:",
-            response.statusText
-          );
-        }
-      })
-      .then((content) => {
-        tinymce.activeEditor.setContent(content);
+    const selectElement = document.getElementById("fileList");
+    const selectedValue = selectElement.value;
+    fetch("/api/access-token")
+      .then((response) => response.json())
+      .then((data) => {
+        let accessToken = data.accessToken;
+        // Now you can use the accessToken in your client-side code
+        fetch("https://content.dropboxapi.com/2/files/download", {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Dropbox-API-Arg": JSON.stringify({
+              // path: "/htmls",
+              path: `/htmls/${selectedValue}`, // Use backticks to interpolate filename
+            }),
+          },
+        })
+          .then((response) => {
+            if (response.ok) {
+              return response.text();
+            } else {
+              console.error(
+                "Error loading file from Dropbox:",
+                response.statusText
+              );
+            }
+          })
+          .then((content) => {
+            tinymce.activeEditor.setContent(content);
+          })
+          .catch((error) => {
+            console.error("Error loading file from Dropbox:", error);
+          });
       })
       .catch((error) => {
-        console.error("Error loading file from Dropbox:", error);
+        console.error("Error:", error);
       });
   });
 
+  // Function to list files from the "htmls" folder on Dropbox
+  function listFilesFromDropbox() {
+    fetch("/api/access-token")
+      .then((response) => response.json())
+      .then((data) => {
+        let accessToken = data.accessToken;
+        fetch("https://api.dropboxapi.com/2/files/list_folder", {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            path: "/htmls",
+          }),
+        })
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            } else {
+              console.error(
+                "Error listing files from Dropbox:",
+                response.statusText
+              );
+            }
+          })
+          .then((data) => {
+            // Assuming that data.entries is an array of file objects
+            const fileNames = data.entries;
+            populateFileList(fileNames);
+          })
+          .catch((error) => {
+            console.error("Error listing files from Dropbox:", error);
+          });
+        // Now you can use the accessToken in your client-side code
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+  // Function to populate the select element with options
+  function populateFileList(fileNames) {
+    const selectElement = document.getElementById("fileList");
+
+    // Clear existing options
+    selectElement.innerHTML = "";
+
+    // Populate options based on the file names from Dropbox
+    fileNames.forEach((fileName) => {
+      const option = document.createElement("option");
+      option.value = fileName.name;
+      option.text = fileName.name;
+      selectElement.appendChild(option);
+    });
+  }
+
+  // Attach the click event listener to the button
+  document
+    .getElementById("refreshButton")
+    .addEventListener("click", listFilesFromDropbox);
+
+  // Initial file list population
+  listFilesFromDropbox();
   // Function to save content
   function saveContent() {
     // Implement save logic here if needed
